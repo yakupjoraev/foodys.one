ce11baebf43fcdb29be590ac28399bfb30d691f7
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-09-23 18:24:06 +0300

    feat: "Search results" page

---
+++ foodys/src/components/RestaurantCard/index.tsx
@@ -0,0 +1,180 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+export interface RestaurantCardProps {
+  name: string;
+  address: string;
+  photo?: string;
+  userRatingTotal: number;
+  rating: number;
+}
+
+export function RestaurantCard(props: RestaurantCardProps) {
+  return (
+    <div className="restaurant">
+      <div className="restaurant__pictures">
+        <Swiper
+          className="restaurant__slider mySwiper"
+          spaceBetween={6}
+          slidesPerView={1}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={{
+            nextEl: ".restaurant__slider-arrow-next",
+            prevEl: ".restaurant__slider-arrow-prev",
+          }}
+          pagination={{
+            el: ".restaurant__slider-paginations",
+            type: "bullets",
+          }}
+          wrapperClass="restaurant__slider-wrapper"
+        >
+          {props.photo && (
+            <SwiperSlide className="restaurant__slide">
+              <img
+                src={props.photo}
+                alt="slide"
+                width="168"
+                height="168"
+                loading="lazy"
+              />
+            </SwiperSlide>
+          )}
+          <SwiperSlide className="restaurant__slide">
+            <img
+              style={{ height: "168px", objectFit: "cover" }}
+              src="/img/dashboard/slide-1.jpg"
+              alt="slide"
+              width="168"
+              height="168"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="restaurant__slide">
+            <img src="/img/dashboard/slide-2.jpg" alt="slide" />
+          </SwiperSlide>
+          <SwiperSlide className="restaurant__slide">
+            <img src="/img/dashboard/slide-3.jpg" alt="slide" />
+          </SwiperSlide>
+          <SwiperSlide className="restaurant__slide">
+            <img src="/img/dashboard/slide-4.jpg" alt="slide" />
+          </SwiperSlide>
+          <SwiperSlide className="restaurant__slide">
+            <img src="/img/dashboard/slide-5.jpg" alt="slide" />
+          </SwiperSlide>
+          <div className="restaurant__slider-arrow restaurant__slider-arrow-prev">
+            <img src="/img/dashboard/arrow-prev.svg" alt="prev" />
+          </div>
+          <div className="restaurant__slider-arrow restaurant__slider-arrow-next">
+            <img src="/img/dashboard/arrow-next.svg" alt="next" />
+          </div>
+          <div className="restaurant__slider-paginations" />
+        </Swiper>
+        <div className="restaurant__favorite">
+          <div className="restaurant__favorite-heart">
+            <svg
+              width={13}
+              height={13}
+              viewBox="0 0 13 13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M0.948692 1.65769C1.55632 1.02354 2.38034 0.667289 3.23953 0.667289C4.09872 0.667289 4.92273 1.02354 5.53036 1.65769L6.47975 2.64798L7.42913 1.65769C7.72803 1.33461 8.08557 1.07691 8.48089 0.899629C8.87621 0.722346 9.30139 0.629031 9.73163 0.625128C10.1619 0.621225 10.5885 0.706813 10.9867 0.876897C11.385 1.04698 11.7467 1.29816 12.051 1.61577C12.3552 1.93338 12.5958 2.31106 12.7587 2.72678C12.9216 3.1425 13.0036 3.58793 12.9999 4.03708C12.9961 4.48623 12.9068 4.93011 12.7369 5.34281C12.5671 5.75551 12.3203 6.12877 12.0108 6.44082L6.47975 12.2159L0.948692 6.44082C0.341245 5.80647 0 4.94622 0 4.04926C0 3.15229 0.341245 2.29204 0.948692 1.65769Z"
+                fill="#A8ADB8"
+              />
+            </svg>
+          </div>
+          <p className="restaurant__favorite-text">Added to favorites</p>
+        </div>
+      </div>
+      <div className="restaurant__top">
+        <div className="restaurant__texts">
+          <h3 className="restaurant__name">{props.name}</h3>
+          <div className="restaurant__tags">
+            <div className="restaurant__tag">#Tagname no.1</div>
+            <div className="restaurant__tag">#Tagname no.2</div>
+            <div className="restaurant__tag">#Tagname no.3</div>
+          </div>
+          <div className="restaurant__address">
+            <div className="restaurant__address-info">
+              <img src="/img/dashboard/geo.svg" alt="geo" />
+              <p>{props.address}</p>
+              <span>–</span>
+            </div>
+            <div className="restaurant__address-gets">
+              <a className="restaurant__address-get" href="#">
+                Get there
+              </a>
+              <div className="restaurant__address-distance">
+                | 835m from you
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="restaurant__checked">
+          <div className="restaurant__checked-label restaurant__checked-label--green">
+            Open now
+          </div>
+          <div className="restaurant__reviews">
+            <div className="restaurant__reviews-balls">
+              {props.rating.toFixed(1)}
+            </div>
+            <div className="restaurant__reviews-stars">
+              <img
+                className="restaurant__reviews-star"
+                src="/img/dashboard/star.svg"
+                alt="star"
+              />
+              <img
+                className="restaurant__reviews-star"
+                src="/img/dashboard/star.svg"
+                alt="star"
+              />
+              <img
+                className="restaurant__reviews-star"
+                src="/img/dashboard/star.svg"
+                alt="star"
+              />
+              <img
+                className="restaurant__reviews-star"
+                src="/img/dashboard/star.svg"
+                alt="star"
+              />
+              <img
+                className="restaurant__reviews-star"
+                src="/img/dashboard/star.svg"
+                alt="star"
+              />
+            </div>
+            <div className="restaurant__reviews-count">
+              ({props.userRatingTotal})
+            </div>
+            <div className="restaurant__reviews-currency"> · €€</div>
+          </div>
+        </div>
+      </div>
+      <div className="restaurant__bottom">
+        <div className="restaurant__btns">
+          <button type="button" className="restaurant__btn call">
+            <img src="/img/dashboard/call.svg" alt="call" />
+            Call
+          </button>
+          <button type="button" className="restaurant__btn delivery">
+            <img src="/img/dashboard/delivery.svg" alt="delivery" />
+            Delivery
+          </button>
+          <button type="button" className="restaurant__btn pay-crypto">
+            <img src="/img/dashboard/pay-crypto.svg" alt="pay-crypto" />
+            Pay in Crypto
+          </button>
+        </div>
+        <a className="restaurant__more" href="#">
+          See more
+        </a>
+      </div>
+    </div>
+  );
+}
--- foodys/src/pages/places.tsx
+++ foodys/src/pages/places.tsx
@@ -1,6 +1,6 @@
-import Head from "next/head";
+import { Layout } from "~/components/Layout";
 import { useSearchParams } from "next/navigation";
-import { RestaurantCard } from "~/components/restaurant-card";
+import { RestaurantCard } from "~/components/RestaurantCard";
 import { api } from "~/utils/api";
 
 export default function Places() {
@@ -10,98 +10,358 @@ export default function Places() {
   const queryResponse = api.places.getPlaces.useQuery({ query: query || "" });
 
   return (
-    <>
-      <Head>
-        <title>Foodys - Places</title>
-        <link rel="stylesheet" href="/css/places.css" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="page">
-        <header className="header">
-          <a className="logo" href="/">
-            foodys.one
-          </a>
-          <div className="header__extra">
-            <button className="login-btn btn hidden">Sign In</button>
-            <button className="logout-btn btn hidden">Sign Out</button>
-            <button className="register-btn btn hidden">Register</button>
-          </div>
-        </header>
-        <div className="page__content listing-page">
-          <div className="listing-page__primary listing">
-            <div className="filter">
-              <select className="filter__option">
-                <option value="none">Establishment type</option>
-              </select>
-              <select className="filter__option">
-                <option value="none">Service</option>
-              </select>
-              <select className="filter__option">
-                <option value="none">Cuisine</option>
-              </select>
-              <select className="filter__option">
-                <option value="none">Rating</option>
-              </select>
-              <select className="filter__option">
-                <option value="none">Price</option>
-              </select>
-              <select className="filter__option">
-                <option value="none">Hours</option>
-              </select>
-              <button className="filter__clear">× Clear</button>
-            </div>
-            <div className="restaurant-cards">
-              {!queryResponse.data && "Loading..."}
-              {!!queryResponse.data &&
-                queryResponse.data.results.length === 0 &&
-                "Not found"}
-              {!!queryResponse.data &&
-                queryResponse.data.results.length > 0 &&
-                queryResponse.data.results.map((candidate) => {
-                  let photo = "https://placekitten.com/200/200";
-                  const candidatePhotos = candidate.photos;
-                  if (candidatePhotos && candidatePhotos.length) {
-                    const firstPhoto = candidatePhotos[0];
-                    const photoReference = firstPhoto?.photo_reference;
-                    if (photoReference) {
-                      photo =
-                        "/api/place-photo?photo_reference=" +
-                        encodeURIComponent(photoReference);
-                    }
-                  }
-                  return (
-                    <RestaurantCard
-                      id={candidate.place_id || ""}
-                      name={candidate.name || ""}
-                      photo={photo}
-                      rating={candidate.rating || 0}
-                      reviewsCount={candidate.user_ratings_total || 0}
-                      address={candidate.formatted_address || ""}
+    <Layout title="Foodys - Search result">
+      <main className="main">
+        <div className="dashboard">
+          <div className="container">
+            <form action="#" className="dashboard__form">
+              <div
+                className="dashboard__form-search search-wrapper"
+                data-search-wrapper=""
+              >
+                <input
+                  className="menu__item-search"
+                  type="search"
+                  placeholder="City, cuisine or restaurant name"
+                  data-search-input=""
+                />
+                <img
+                  className="menu__item-search-icon"
+                  src="/img/icons/glass.svg"
+                  alt=""
+                />
+                <div className="search__list">
+                  <a className="search__item" href="#">
+                    <img
+                      className="search__item-pic"
+                      src="/img/dashboard/slide-1.jpg"
+                      alt=""
                     />
-                  );
-                })}
+                    <div className="search__item-info">
+                      <h4 className="search__item-label">
+                        Hank Burger Paris Archives
+                      </h4>
+                      <div className="search__item-address">
+                        <img src="/img/dashboard/geo.svg" alt="" />
+                        <p>Paris, lle-de-France, France</p>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="search__item" href="#">
+                    <img
+                      className="search__item-pic"
+                      src="/img/dashboard/slide-1.jpg"
+                      alt=""
+                    />
+                    <div className="search__item-info">
+                      <h4 className="search__item-label">
+                        Hank Burger Paris Archives
+                      </h4>
+                      <div className="search__item-address">
+                        <img src="/img/dashboard/geo.svg" alt="" />
+                        <p>Paris, lle-de-France, France</p>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="search__item" href="#">
+                    <img
+                      className="search__item-pic"
+                      src="/img/dashboard/slide-1.jpg"
+                      alt=""
+                    />
+                    <div className="search__item-info">
+                      <h4 className="search__item-label">
+                        Hank Burger Paris Archives
+                      </h4>
+                      <div className="search__item-address">
+                        <img src="/img/dashboard/geo.svg" alt="" />
+                        <p>Paris, lle-de-France, France</p>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="search__item" href="#">
+                    <img
+                      className="search__item-pic"
+                      src="/img/dashboard/slide-1.jpg"
+                      alt=""
+                    />
+                    <div className="search__item-info">
+                      <h4 className="search__item-label">
+                        Hank Burger Paris Archives
+                      </h4>
+                      <div className="search__item-address">
+                        <img src="/img/dashboard/geo.svg" alt="" />
+                        <p>Paris, lle-de-France, France</p>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="search__item" href="#">
+                    <img
+                      className="search__item-pic"
+                      src="/img/dashboard/slide-1.jpg"
+                      alt=""
+                    />
+                    <div className="search__item-info">
+                      <h4 className="search__item-label">
+                        Hank Burger Paris Archives
+                      </h4>
+                      <div className="search__item-address">
+                        <img src="/img/dashboard/geo.svg" alt="" />
+                        <p>Paris, lle-de-France, France</p>
+                      </div>
+                    </div>
+                  </a>
+                  <a className="search__more" href="#">
+                    View all results <span>(234)</span>
+                  </a>
+                </div>
+              </div>
+              <div className="dashboard__form-results">
+                <p className="dashboard__form-results-count">80 resultats </p>
+                <p className="dashboard__form-results-name">
+                  pour “Restaurants Paris Neobistror”
+                </p>
+              </div>
+              <div className="dashboard__filters">
+                <div className="dashboard__filter dashboard__filter--square">
+                  <p className="dashboard__filter-text">Establishment type</p>
+                  <button type="button" className="dashboard__filter-btn">
+                    <img
+                      src="/img/dashboard/filter-arrow.svg"
+                      alt="filter arrow"
+                    />
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--square">
+                  <p className="dashboard__filter-text">Service</p>
+                  <button type="button" className="dashboard__filter-btn">
+                    <img
+                      src="/img/dashboard/filter-arrow.svg"
+                      alt="filter arrow"
+                    />
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--square">
+                  <p className="dashboard__filter-text">Cuisine</p>
+                  <button type="button" className="dashboard__filter-btn">
+                    <img
+                      src="/img/dashboard/filter-arrow.svg"
+                      alt="filter arrow"
+                    />
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--square">
+                  <p className="dashboard__filter-text">Rating</p>
+                  <button type="button" className="dashboard__filter-btn">
+                    <img
+                      src="/img/dashboard/filter-arrow.svg"
+                      alt="filter arrow"
+                    />
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--square">
+                  <p className="dashboard__filter-text">Price</p>
+                  <button type="button" className="dashboard__filter-btn">
+                    <img
+                      src="/img/dashboard/filter-arrow.svg"
+                      alt="filter arrow"
+                    />
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--square">
+                  <p className="dashboard__filter-text">Hours</p>
+                  <button type="button" className="dashboard__filter-btn">
+                    <img
+                      src="/img/dashboard/filter-arrow.svg"
+                      alt="filter arrow"
+                    />
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--clear">
+                  <button type="button" className="dashboard__filter-btn">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={8}
+                      height={8}
+                      viewBox="0 0 8 8"
+                      fill="none"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M3.8999 4.80916L7.09082 8.00008L7.79793 7.29297L4.60701 4.10205L7.7998 0.909256L7.0927 0.202148L3.8999 3.39494L0.707107 0.202148L0 0.909256L3.1928 4.10205L0.00187718 7.29297L0.708984 8.00008L3.8999 4.80916Z"
+                        fill="#A8ADB8"
+                      />
+                    </svg>{" "}
+                    Clear all
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--filters">
+                  <img src="/img/dashboard/filters.svg" alt="filters" />
+                  <p className="dashboard__filter-text">Filtres</p>
+                  <button type="button" className="dashboard__filter-btn">
+                    <img
+                      src="/img/dashboard/filter-arrow.svg"
+                      alt="filter arrow"
+                    />
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--sort">
+                  <img src="/img/dashboard/sort.svg" alt="sort view" />
+                  <p className="dashboard__filter-text">Sort by</p>
+                  <button type="button" className="dashboard__filter-btn">
+                    <img
+                      src="/img/dashboard/filter-arrow.svg"
+                      alt="filter arrow"
+                    />
+                  </button>
+                </div>
+                <div className="dashboard__filter dashboard__filter--position">
+                  <p className="dashboard__filter-text">Position display</p>
+                  <div className="dashboard__filter-positions">
+                    <div className="dashboard__filter-position-selected">
+                      10
+                      <img
+                        src="/img/dashboard/position-arrow.svg"
+                        alt="position arrow"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+            <div className="dashboard__main">
+              <div className="restaurants">
+                {!queryResponse.data && "Loading..."}
+                {!!queryResponse.data &&
+                  queryResponse.data.results.length === 0 &&
+                  "Not found"}
+                {!!queryResponse.data &&
+                  queryResponse.data.results.length > 0 &&
+                  queryResponse.data.results.map((candidate) => {
+                    if (!candidate.place_id) {
+                      return null;
+                    }
+                    return (
+                      <RestaurantCard
+                        name={candidate.name || "???"}
+                        address={candidate.formatted_address || "???"}
+                        rating={candidate.rating || 0}
+                        userRatingTotal={candidate.user_ratings_total || 0}
+                        key={candidate.place_id}
+                      />
+                    );
+                  })}
+              </div>
+              <aside className="dashboard__aside">
+                <a className="dashboard__aside-link" href="#">
+                  <img
+                    src="/img/dashboard/aside-pic.png"
+                    alt="aside pic reclame"
+                  />
+                </a>
+                <div className="dashboard__aside-footer">
+                  <p className="dashboard__aside-text">As an advertisement</p>
+                  <a className="dashboard__aside-more" target="_blank" href="#">
+                    Learn more
+                  </a>
+                </div>
+              </aside>
             </div>
-          </div>
-          <div className="listing-page__secondary">
-            <div className="position-display">
-              <label
-                className="position-display__label"
-                htmlFor="position-display"
-              >
-                Position display
-              </label>
-              <select
-                className="position-display__select"
-                id="position-display"
-              >
-                <option value={10}>10</option>
-                <option value={20}>20</option>
-                <option value={30}>30</option>
-              </select>
+            <div className="nav-lists">
+              <a className="nav-lists__btn" href="#">
+                Previous
+              </a>
+              <ul className="nav-lists__list">
+                <li className="nav-lists__item active">
+                  <a className="nav-lists__link " href="#">
+                    1
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    2
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    3
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    4
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    5
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    6
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    7
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    8
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    9
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    10
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    11
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    12
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    13
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    14
+                  </a>
+                </li>
+                <li className="nav-lists__item">
+                  <a className="nav-lists__link" href="#">
+                    15
+                  </a>
+                </li>
+                <li className="nav-lists__item dots">•••</li>
+                <li className="nav-lists__item nav-lists__item--end">
+                  <a className="nav-lists__link" href="#">
+                    33
+                  </a>
+                </li>
+              </ul>
+              <a className="nav-lists__btn active" href="#">
+                Next
+              </a>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </main>
+    </Layout>
   );
 }
