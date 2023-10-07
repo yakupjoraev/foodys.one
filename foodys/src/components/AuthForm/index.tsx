e9b4c0067444ed4f4097e239611d380da96314b5
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-10-07 21:21:23 +0300

    feat: translate Auth modal

--- foodys/src/components/AuthForm/index.tsx
+++ foodys/src/components/AuthForm/index.tsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { authFormSchema } from "./validators";
 import { toast } from "react-hot-toast";
+import Trans from "next-translate/Trans";
+import useTranslation from "next-translate/useTranslation";
 
 export type AuthError =
   | { type: "unknown"; message?: string }
@@ -32,6 +34,7 @@ export interface AuthFormProps {
 }
 
 export function AuthForm(props: AuthFormProps) {
+  const { t } = useTranslation("common");
   const loginId = useId();
   const passwordId = useId();
   const {
@@ -71,13 +74,18 @@ export function AuthForm(props: AuthFormProps) {
   return (
     <form className="modal-content__form" onSubmit={handleAuthFormSubmit}>
       <div className="modal-content__form-top">
-        <h3 className="modal-content__title">Already a member?</h3>
-        <h2 className="modal-content__title-main">Sign in!</h2>
+        <Trans
+          i18nKey="common:titleSignInForm"
+          components={[
+            <h3 className="modal-content__title" />,
+            <h2 className="modal-content__title-main" />,
+          ]}
+        />
       </div>
       <div className="modal-content__inputs">
         <div className="input__group">
           <label className="input__label" htmlFor={loginId}>
-            Email*
+            {t("fieldNameEmail")}
           </label>
           <input
             className="input"
@@ -93,12 +101,12 @@ export function AuthForm(props: AuthFormProps) {
         </div>
         <div className="input__group">
           <label className="input__label" htmlFor={passwordId}>
-            Password*
+            {t("fieldNamePassword")}
           </label>
           <input
             className="input"
             type="password"
-            placeholder="   Password*"
+            placeholder={t("fieldNamePassword")}
             id={passwordId}
             disabled={props.loading}
             {...register("password")}
@@ -115,7 +123,7 @@ export function AuthForm(props: AuthFormProps) {
               toast("NOT IMPLEMENTED");
             }}
           >
-            Forgotten your password?
+            {t("textForgottenPwd")}
           </a>
         </div>
         {errors.root?.message && (
@@ -128,29 +136,28 @@ export function AuthForm(props: AuthFormProps) {
         type="submit"
         disabled={props.loading}
       >
-        Sign in
+        {t("buttonSignIn")}
       </button>
       <div className="modal-content__btn-remember">
-        New over here?
-        <a
-          href="#"
-          target="_blank"
-          onClick={(ev) => {
-            ev.preventDefault();
-            toast("NOT IMPLEMENTED");
-          }}
-        >
-          Create your account in 20 seconds !
-        </a>
+        <Trans
+          i18nKey="common:textCreateAccountInvit"
+          components={[
+            <a
+              href="#"
+              target="_blank"
+              onClick={(ev) => {
+                ev.preventDefault();
+                toast("NOT IMPLEMENTED");
+              }}
+            />,
+          ]}
+        />
       </div>
       <div className="input__checkbox-group">
         <input className="input__checkbox" type="checkbox" id="checkbox1" />
         <label className="input__label" htmlFor="checkbox1">
           <div className="input__checkbox-decor"> </div>
-          <span>
-            By proceeding, you agree to our Terms of Use and confirm you have
-            read our Privacy and Cookies Statement.
-          </span>
+          <span>{t("textAgreeTermsAndCond")}</span>
         </label>
       </div>
     </form>
