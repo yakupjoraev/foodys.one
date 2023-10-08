364e944b6aa8f1c974a0d7d654ce38cb90452e7b
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-10-08 12:36:31 +0300

    feat: cache places

--- foodys/src/server/api/routers/place.ts
+++ foodys/src/server/api/routers/place.ts
@@ -2,12 +2,26 @@ import { z } from "zod";
 import { env } from "~/env.mjs";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { gmClient } from "~/server/gm-client";
+import { Place } from "~/server/gm-client/types";
+import { dnull } from "dnull";
+import { Prisma } from "@prisma/client";
 
 export const placeRouter = createTRPCRouter({
   getPlace: publicProcedure
     .input(z.object({ placeId: z.string() }))
-    .query(async ({ input }) => {
+    .query(async ({ input, ctx }): Promise<Place | null> => {
       const placeId = input.placeId;
+
+      const existsPlace = await ctx.db.gPlace.findFirst({
+        where: {
+          place_id: placeId,
+        },
+      });
+
+      if (existsPlace !== null) {
+        const { created_at, updated_at, ...rest } = existsPlace;
+        return dnull(rest);
+      }
 
       const placeDetails = await gmClient.placeDetails({
         queries: {
@@ -16,6 +30,30 @@ export const placeRouter = createTRPCRouter({
         },
       });
 
-      return placeDetails.result || null;
+      const fetchedPlace = placeDetails.result;
+      if (!fetchedPlace) {
+        return null;
+      }
+
+      if (fetchedPlace.place_id === undefined) {
+        return null;
+      }
+
+      try {
+        await ctx.db.gPlace.create({
+          data: fetchedPlace,
+        });
+      } catch (error) {
+        if (
+          error instanceof Prisma.PrismaClientKnownRequestError &&
+          error.code === "P2002"
+        ) {
+          return fetchedPlace;
+        } else {
+          throw error;
+        }
+      }
+
+      return fetchedPlace;
     }),
 });
