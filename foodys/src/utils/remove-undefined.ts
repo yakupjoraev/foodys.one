d41f222ec21a839a6b8d5c1bcbb2cf0c5a31809f
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-10-27 17:19:58 +0300

    refactor: ignore eslint rules

--- foodys/src/utils/remove-undefined.ts
+++ foodys/src/utils/remove-undefined.ts
@@ -1,24 +1,29 @@
 export function removeUndefined<T>(src: T): T {
   if (src === null) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
     return undefined as any;
   }
   if (src === undefined) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
     return undefined as any;
   }
   if (Array.isArray(src)) {
     for (let i = 0; i < src.length; i++) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       src[i] = removeUndefined(src[i]);
     }
   } else if (src.constructor.name === "Object") {
-    for (let key in src) {
+    for (const key in src) {
       const val = src[key];
       if (val === undefined) {
         delete src[key];
       } else {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment,
         src[key] = removeUndefined(src[key]) as any;
       }
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
   return src as any;
 }
