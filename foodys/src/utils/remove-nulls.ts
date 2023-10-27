2ef63a1316c7845b64a29147eb4bfd62a5dcb630
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-10-27 17:17:01 +0300

    refactor: ignore eslint rules

--- foodys/src/utils/remove-nulls.ts
+++ foodys/src/utils/remove-nulls.ts
@@ -10,25 +10,30 @@ type WithoutNulls<T> = T extends null
 
 export function removeNulls<T>(src: T): WithoutNulls<T> {
   if (src === null) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
     return undefined as any;
   }
   if (src === undefined) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
     return undefined as any;
   }
   if (Array.isArray(src)) {
     for (let i = 0; i < src.length; i++) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       src[i] = removeNulls(src[i]);
     }
   } else if (src.constructor.name === "Object") {
-    for (let key in src) {
+    for (const key in src) {
       const val = src[key];
       if (val === null) {
         delete src[key];
       } else {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment,
         src[key] = removeNulls(src[key]) as any;
       }
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
   return src as any;
 }
