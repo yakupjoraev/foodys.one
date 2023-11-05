ade1cb0d9f9eae776c5ec42bc73028b81b12e33e
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-11-05 18:29:26 +0200

    feat: hi-res presets

--- foodys-photos/src/config/presets.ts
+++ foodys-photos/src/config/presets.ts
@@ -1,12 +1,14 @@
 import { Sharp } from "sharp";
 
-export type PresetId = "cover_168x168";
+export type PresetId = "cover_168x168" | "cover_336x336" | "cover_672x672";
 
 export type Presets = Record<PresetId, (sharp: Sharp) => Sharp>;
 
 export function isPresetId(val: string): val is PresetId {
   switch (val) {
-    case "cover_168x168": {
+    case "cover_168x168":
+    case "cover_336x336":
+    case "cover_672x672": {
       return true;
     }
     default: {
@@ -17,4 +19,6 @@ export function isPresetId(val: string): val is PresetId {
 
 export const presets: Presets = {
   cover_168x168: (sharp) => sharp.resize({ width: 168, height: 168 }),
+  cover_336x336: (sharp) => sharp.resize({ width: 336, height: 336 }),
+  cover_672x672: (sharp) => sharp.resize({ width: 672, height: 672 }),
 };
