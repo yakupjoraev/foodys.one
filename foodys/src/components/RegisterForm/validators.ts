50cd6f315ca37d2e30bd09761a695bedb5a37f85
Bogdan Kapustin <goldbranch.dev@gmail.com>
2023-11-14 12:48:03 +0200

    feat: validation errors of the sign up form

--- foodys/src/components/RegisterForm/validators.ts
+++ foodys/src/components/RegisterForm/validators.ts
@@ -2,22 +2,26 @@ import { passwordStrength } from "check-password-strength";
 import { object, optional, string, literal } from "zod";
 
 export const registerFormSchema = object({
-  firstName: string().nonempty().max(256),
-  lastName: string().nonempty().max(256),
+  firstName: string()
+    .min(1, "This field is required")
+    .max(256, "This field must have a maximum of 256 characters"),
+  lastName: string()
+    .min(1, "This field is required")
+    .max(256, "This field must have a maximum of 256 characters"),
   nickname: string()
-    .max(256)
-    .regex(/^[a-z0-9_]*$/, "allowed symbols: a-z, 0-9 and _"),
+    .max(256, "This field must have a maximum of 256 characters")
+    .regex(/^[a-z0-9_]*$/, "Allowed symbols: a-z, 0-9 and _"),
   email: string().email(),
   password: string()
-    .min(8)
-    .max(256)
+    .min(8, "This field must have a minimum of 8 characters")
+    .max(256, "This field must have a maximum of 256 characters")
     .refine(
       (val) => {
         return passwordStrength(val).id > 1;
       },
       {
         message:
-          "Requires characters from the ranges a–z, A–Z, 0–9, and special symbols.",
+          "Requires characters from the ranges a–z, A–Z, 0–9, and special symbols",
       }
     ),
   passwordConfirm: string(),
