78c33783a80dcbf9dd3cbb705a92bba50bc87d12
Yakup <yakupjoraev@gmail.com>
2024-02-11 01:38:45 +0300

    added 404 page

--- foodys/src/pages/404.tsx
+++ foodys/src/pages/404.tsx
@@ -2,9 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <p>
-      Not found <br />
-      <Link href="/">Go home</Link>
-    </p>
+    <div className="not-found">
+      <div className="not-found__inner">
+        <div className="not-found__sum">404</div>
+        <p className="not-found__text">Page no found</p>
+
+        <Link className="not-found__link" href="/">GO TO HOME PAGE</Link>
+      </div>
+
+    </div>
   );
 }
