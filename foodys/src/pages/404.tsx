afc930956e9aea0a4075bf45f1f8f2727fe8476b
Bogdan Kapustin <goldbranch.dev@gmail.com>
2024-02-11 12:29:06 +0200

    feat: translate 404 page

--- foodys/src/pages/404.tsx
+++ foodys/src/pages/404.tsx
@@ -1,15 +1,19 @@
+import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 
 export default function NotFound() {
+  const { t } = useTranslation("common");
+
   return (
     <div className="not-found">
       <div className="not-found__inner">
         <div className="not-found__sum">404</div>
-        <p className="not-found__text">Page no found</p>
+        <p className="not-found__text">{t("textPageNotFound")}</p>
 
-        <Link className="not-found__link" href="/">GO TO HOME PAGE</Link>
+        <Link className="not-found__link" href="/">
+          {t("buttonGoToHomePage")}
+        </Link>
       </div>
-
     </div>
   );
 }
