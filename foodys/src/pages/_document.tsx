989099d1ec573802a7678aa09f9618312ce1feb4
Bogdan Kapustin <goldbranch.dev@gmail.com>
2024-02-13 22:59:49 +0200

    feat: viewport meta tag

--- foodys/src/pages/_document.tsx
+++ foodys/src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default function Document() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fea934"/>
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" href="/css/swiper-bundle.min.css" />
       </Head>
       <body>
