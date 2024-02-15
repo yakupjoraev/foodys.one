23ddac2185a6aa802807de861e7740dc6cb4a532
Bogdan Kapustin <goldbranch.dev@gmail.com>
2024-02-15 15:31:23 +0200

    refactor: satisfy @typescript-eslint/consistent-type-imports

--- foodys/src/components/AboutSearch/index.tsx
+++ foodys/src/components/AboutSearch/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { FormEvent, useId } from "react";
+import { type FormEvent, useId } from "react";
 import useTranslation from "next-translate/useTranslation";
 
 export function AboutSearch() {
