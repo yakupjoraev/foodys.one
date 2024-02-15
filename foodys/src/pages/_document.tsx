52383b4be11fa78df61a8f68583077b605876985
Bogdan Kapustin <goldbranch.dev@gmail.com>
2024-02-15 17:26:26 +0200

    refactor: format

--- foodys/src/pages/_document.tsx
+++ foodys/src/pages/_document.tsx
@@ -4,17 +4,32 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fea934"/>
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="theme-color" content="#ffffff"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fea934" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {
           // eslint-disable-next-line @next/next/no-css-tags
-        }<link rel="stylesheet" href="/css/swiper-bundle.min.css" />
+          <link rel="stylesheet" href="/css/swiper-bundle.min.css" />
+        }
       </Head>
       <body>
         <Main />
