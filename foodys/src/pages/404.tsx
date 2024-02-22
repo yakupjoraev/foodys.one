872cc811218a7117053187c2f1dd7f140bcc9eb2
Yakup <yakupjoraev@gmail.com>
2024-02-22 17:30:54 +0300

    added logo for 404 and scroll for restor

--- foodys/src/pages/404.tsx
+++ foodys/src/pages/404.tsx
@@ -7,6 +7,9 @@ export default function NotFound() {
   return (
     <div className="not-found">
       <div className="not-found__inner">
+        <a href="/" className="not-found__logo">
+          <img width="400" src="/img/icons/foodys-logo.svg" alt="" />
+        </a>
         <div className="not-found__sum">404</div>
         <p className="not-found__text">{t("textPageNotFound")}</p>
 
